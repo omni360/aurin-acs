c73b01a7c9fff27db350f0f599ade18824e2a344
Oliver Lade <piemaster21@gmail.com>
2014-09-26 14:02:55 +1000

    Improved index page, linking to repo.

--- src/main/java/au/com/mutopia/acs/resources/MainResource.java
+++ src/main/java/au/com/mutopia/acs/resources/MainResource.java
@@ -5,13 +5,23 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * Serves HTML for the index page.
+ */
 @Path("/")
 @Produces(MediaType.TEXT_HTML)
 public class MainResource {
-  
+
+  /** The URL to the source code repository. */
+  private static final String REPO_URL = "https://bitbucket.org/mutopia/aurin-acs";
+
+  /**
+   * Serves the HTML for browsers on the index page.
+   */
   @GET
   public String main() {
-    return "<b>Hello</b>";
+    return "ACS does not have a user interface. For instructions, see the <a href=\"" + REPO_URL
+        + "\">source code repository</a>.";
   }
 
 }
