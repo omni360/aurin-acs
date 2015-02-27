332e6ded9511fa2becee3b20949e32da982f041d
Aram Kocharyan <akarmenia@gmail.com>
2015-02-27 16:19:04 +1100

    Fixed a bug causing ogr2ogr to fail if given a shapefile with spaces.

--- src/main/java/au/com/mutopia/acs/util/Ogr2Ogr.java
+++ src/main/java/au/com/mutopia/acs/util/Ogr2Ogr.java
@@ -1,6 +1,7 @@
 package au.com.mutopia.acs.util;
 
 import au.com.mutopia.acs.exceptions.ConversionException;
+import com.google.common.collect.ImmutableMap;
 import lombok.extern.log4j.Log4j;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
@@ -26,19 +27,27 @@ public class Ogr2Ogr {
    */
   public static File convertToKml(File file) throws ConversionException {
     String outPath = TEMP_DIR + "/" + UUID.randomUUID() + ".kml";
-    String command = String.format("ogr2ogr -f KML \"%s\" \"%s\"", outPath, file.getAbsolutePath());
 
-    CommandLine cmdLine = CommandLine.parse(command);
-    DefaultExecutor executor = new DefaultExecutor();
-    log.debug("Executing command: " + command);
+    // Build the command.
+    CommandLine cmdLine = new CommandLine("ogr2ogr");
+    cmdLine.addArgument("-f");
+    cmdLine.addArgument("KML");
+    cmdLine.addArgument("${out}");
+    // TODO(aramk) Without the false argument, quotes are added if the path contains spaces, and
+    // this causes ogr2ogr to fail since double quotes are passed.
+    cmdLine.addArgument("${in}", false);
+    cmdLine.setSubstitutionMap(ImmutableMap.of("in", file, "out", outPath));
+
+    // Execute the command with the system environment, and handle errors.
     try {
-      int exitValue = executor.execute(cmdLine, System.getenv());
+      log.debug("Executing command: " + cmdLine.toString());
+      int exitValue = new DefaultExecutor().execute(cmdLine, System.getenv());
       if (exitValue != 0) {
-        throw new ConversionException("ogr2ogr returned exit code " + exitValue);
+        throw new RuntimeException("ogr2ogr returned exit code " + exitValue);
       }
     } catch (IOException e) {
-      throw new ConversionException("Failed to perform ogr2ogr conversion of "
-          + file.getAbsolutePath() + " (" + command + ")", e);
+      throw new RuntimeException("Failed to convert " + file.getAbsolutePath()
+          + " with ogr2ogr", e);
     }
     return new File(outPath);
   }
