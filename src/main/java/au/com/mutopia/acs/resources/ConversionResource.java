6c7637e182e91cdd45b937fb8913411ca7932c8c
Aram Kocharyan <akarmenia@gmail.com>
2015-04-07 22:44:24 +1000

    Added MIME type and attachment to the return of export() to start a download in the browser.

--- src/main/java/au/com/mutopia/acs/resources/ConversionResource.java
+++ src/main/java/au/com/mutopia/acs/resources/ConversionResource.java
@@ -1,10 +1,19 @@
 package au.com.mutopia.acs.resources;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
+import au.com.mutopia.acs.conversion.Converter;
+import au.com.mutopia.acs.conversion.ConverterMap;
+import au.com.mutopia.acs.conversion.output.KmzWriter;
+import au.com.mutopia.acs.exceptions.ConversionException;
+import au.com.mutopia.acs.models.Asset;
+import au.com.mutopia.acs.models.c3ml.C3mlData;
+import au.com.mutopia.acs.models.c3ml.C3mlEntity;
+import com.google.inject.Inject;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+import lombok.extern.log4j.Log4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.time.StopWatch;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
@@ -13,24 +22,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-
-import lombok.extern.log4j.Log4j;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.time.StopWatch;
-
-import au.com.mutopia.acs.conversion.Converter;
-import au.com.mutopia.acs.conversion.ConverterMap;
-import au.com.mutopia.acs.conversion.output.KmzWriter;
-import au.com.mutopia.acs.exceptions.ConversionException;
-import au.com.mutopia.acs.models.Asset;
-import au.com.mutopia.acs.models.c3ml.C3mlData;
-import au.com.mutopia.acs.models.c3ml.C3mlEntity;
-
-import com.google.inject.Inject;
-import com.sun.jersey.core.header.FormDataContentDisposition;
-import com.sun.jersey.multipart.FormDataParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
 
 /**
  * Implements the REST API for file conversion.
@@ -41,12 +40,13 @@ public class ConversionResource {
 
   /** A map of {@link Converter} objects for each available input format. */
   private ConverterMap converters;
+  private static final String KMZ_MIME_TYPE = "application/vnd.google-earth.kmz";
 
   /**
    * Creates the resource with the map of available converters injected.
-   * 
+   *
    * @param converters A map of each supported format to the {@link Converter} used to convert files
-   *        of that format.
+   * of that format.
    */
   @Inject
   public ConversionResource(ConverterMap converters) {
@@ -55,7 +55,7 @@ public class ConversionResource {
 
   /**
    * Converts the given file to the requested target format.
-   * 
+   *
    * @param inputStream A stream of the uploaded file data.
    * @param fileDetail Metadata about the uploaded file.
    * @param merge Whether to merge all entities into one (if possible).
@@ -94,7 +94,7 @@ public class ConversionResource {
 
   /**
    * Converts the given {@link C3mlData} document into a KMZ file, and returns the bytes.
-   * 
+   *
    * @param data The C3ML to export.
    * @return The bytes of the resulting KMZ file.
    * @throws ConversionException if the export fails.
@@ -102,10 +102,19 @@ public class ConversionResource {
   @POST
   @Path("/export")
   @Consumes(MediaType.APPLICATION_JSON)
-  public byte[] export(C3mlData data) throws ConversionException {
+  @Produces(MediaType.APPLICATION_OCTET_STREAM)
+  public Response export(C3mlData data) throws ConversionException {
     File kmzFile = new KmzWriter().convert(data);
     try {
-      return IOUtils.toByteArray(new FileInputStream(kmzFile));
+      final byte[] byteData = IOUtils.toByteArray(new FileInputStream(kmzFile));
+      StreamingOutput output = new StreamingOutput() {
+        @Override
+        public void write(OutputStream output) throws IOException, WebApplicationException {
+          output.write(byteData);
+        }
+      };
+      return Response.ok(output).type(KMZ_MIME_TYPE)
+          .header("content-disposition", "attachment; filename = " + kmzFile.getName()).build();
     } catch (IOException e) {
       throw new ConversionException("Failed to read KMZ file", e);
     }
